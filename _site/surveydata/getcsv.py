ece4f9af2f3f2273b3ac3dd8820bf2a2000a25c3
Pradeep Niroula <zenithworkspace@gmail.com>
2016-10-28 00:33:29 -0400

    basic HTML styling

---
+++ _site/surveydata/getcsv.py
@@ -0,0 +1,8 @@
+#### Import SQL Library
+
+### Connect to the database
+
+### function get_election_preference( crosstab_by ):
+###             return election prefernces crosstabbed by parameter
+
+### other helpful functions
